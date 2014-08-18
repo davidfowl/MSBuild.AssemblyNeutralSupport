7a32e01fa05726f41c2ca2a1fec6ed4ced242c0f
David Fowler <davidfowl@gmail.com>
2014-08-18 01:34:18 -0700

    Added end to end sample showing EF in a regular console application.

---
+++ samples/ConsoleApplication/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Entity;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            using (var db = new MyContext())
+            {
+                db.Add(new Blog { BlogId = 1, Name = "ADO.NET", Url = "http://blogs.msdn.com/adonet" });
+                db.Add(new Blog { BlogId = 2, Name = ".NET Framework", Url = "http://blogs.msdn.com/dotnet" });
+                db.Add(new Blog { BlogId = 3, Name = "Visual Studio", Url = "http://blogs.msdn.com/visualstudio" });
+                db.SaveChanges();
+            }
+
+            using (var db = new MyContext())
+            {
+                var blogs = db.Blogs.OrderBy(b => b.Name);
+                foreach (var item in blogs)
+                {
+                    Console.WriteLine(item.Name);
+                }
+            }
+        }
+    }
+
+    public class MyContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptions builder)
+        {
+            builder.UseInMemoryStore();
+        }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+
+}
